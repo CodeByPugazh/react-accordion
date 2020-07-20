874a0b1df93b1b192b8394d99c5921a7c00634a0
Pugazhenthi <pugazhonline@gmail.com>
2020-07-20 12:08:27 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+} from "react-accessible-accordion";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Accordion example using React Accessible Accordion</h3>
+      <Accordion preExpanded={["one"]}>
+        <AccordionItem uuid="one">
+          <AccordionItemHeading>
+            <AccordionItemButton>React</AccordionItemButton>
+          </AccordionItemHeading>
+          <AccordionItemPanel>
+            <p>A JavaScript library for building user interfaces</p>
+            <p>
+              React makes it painless to create interactive UIs. Design simple
+              views for each state in your application, and React will
+              efficiently update and render just the right components when your
+              data changes.
+            </p>
+          </AccordionItemPanel>
+        </AccordionItem>
+        <AccordionItem uuid="two">
+          <AccordionItemHeading>
+            <AccordionItemButton>Vue</AccordionItemButton>
+          </AccordionItemHeading>
+          <AccordionItemPanel>
+            <p>The Progressive JavaScript Framework</p>
+            <p>
+              An incrementally adoptable ecosystem that scales between a library
+              and a full-featured framework.
+            </p>
+          </AccordionItemPanel>
+        </AccordionItem>
+        <AccordionItem uuid="three">
+          <AccordionItemHeading>
+            <AccordionItemButton>Angular</AccordionItemButton>
+          </AccordionItemHeading>
+          <AccordionItemPanel>
+            <p>One framework. Mobile &amp; desktop.</p>
+            <p>Achieve the maximum speed possible on the Web Platform today, and take it further, via Web Workers and server-side rendering.</p>
+          </AccordionItemPanel>
+        </AccordionItem>
+      </Accordion>
     </div>
   );
 }
